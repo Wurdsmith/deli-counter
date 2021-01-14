6389c7bec09132d7c6b494cff014de8f2fe9c71b
Wurdsmith <wurdsmithing@gmail.com>
2021-01-13 21:07:00 -0600

    commit tree e8bc318c2a82c3cb25b016132da943e4516f2155

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+katz_deli = []
+
+def line(array)
+    if array.count == 0
+        puts "The line is currently empty."
+    else
+        announce = "The line is currently:"
+        array.each_with_index do |name,index|
+        announce += " #{index + 1}. #{name}"
+        end
+        puts announce
+    end
+end
+        
+
+def take_a_number(array,name)
+    array.push(name)
+    puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+
+def now_serving(array)
+    if array.length == 0
+        puts "There is nobody waiting to be served!"
+        
+    else
+        puts "Currently serving #{array.shift}."
+    end
+end
+
